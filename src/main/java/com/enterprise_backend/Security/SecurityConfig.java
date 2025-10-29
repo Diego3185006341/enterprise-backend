3dd4d885b9a21454ba2adab10cd46a149eb59111
Diego <yeogalindo@gmail.com>
2025-10-29 18:54:53 -0500

    Permitir CORS desde frontend en producción

--- src/main/java/com/enterprise_backend/Security/SecurityConfig.java
+++ src/main/java/com/enterprise_backend/Security/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://enterprise-front-production.up.railway.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // si usas cookies o sesiones
