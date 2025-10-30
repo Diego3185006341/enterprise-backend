48aa46bb30ade364fc8d84ef7b062ee2f10f8cd1
Diego <yeogalindo@gmail.com>
2025-10-30 09:20:04 -0500

    dsfsd

--- src/main/java/com/enterprise_backend/Contoller/ProductoController.java
+++ src/main/java/com/enterprise_backend/Contoller/ProductoController.java
@@ -15,7 +15,8 @@ import java.util.List;
         "http://localhost:5173",
         "https://enterprise-front-tau.vercel.app",
         "https://enterprise-front-git-master-diegos-projects-f8b66fa6.vercel.app",
-        "https://enterprise-front-n8n5ig5ao-diegos-projects-f8b66fa6.vercel.app"
+        "https://enterprise-front-n8n5ig5ao-diegos-projects-f8b66fa6.vercel.app",
+        "enterprise-front-z2jz.vercel.app"
 }, allowCredentials = "true")
 public class ProductoController {
     private final ProductoService service;
